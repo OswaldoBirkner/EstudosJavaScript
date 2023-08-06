7d61f1311797b94d3f55c257b0e8379528e6040f
Rafa Friedel <rafafriedeldev@outlook.com>
2023-08-06 11:13:40 -0300

    fix: remove dead codes

--- Ravin/script.js
+++ Ravin/script.js
@@ -9,40 +9,6 @@ function pegarItens() {
         });
 }
 
-// const dataMock = [{
-//     "id": 1,
-//     "categoria": "pratos salgados",
-//     "nome": "Hambúrguer gourmet",
-//     "imagem": "url_da_imagem1.jpg",
-//     "descricao": "Delicioso hambúrguer gourmet com carne suculenta, queijo derretido e vegetais frescos.",
-//     "valor": 20.50
-//   },{
-//     "id": 11,
-//     "categoria": "pratos doces",
-//     "nome": "Cheesecake de frutas vermelhas",
-//     "imagem": "url_da_imagem11.jpg",
-//     "descricao": "Deliciosa cheesecake com cobertura de frutas vermelhas.",
-//     "valor": 22.50
-//   },{
-//     "id": 21,
-//     "categoria": "bebidas alcoolicas",
-//     "nome": "Margarita",
-//     "imagem": "url_da_imagem21.jpg",
-//     "descricao": "Coquetel refrescante de tequila, licor de laranja e suco de limão.",
-//     "valor": 16.50
-//   },{
-//     "id": 40,
-//     "categoria": "bebidas não alcoolicas",
-//     "nome": "Limão soda",
-//     "imagem": "url_da_imagem40.jpg",
-//     "descricao": "Bebida não alcoólica com sabor de limão.",
-//     "valor": 4.00
-//   }
-
-// ]
-
-// const pegarItens = () => montarMenu(dataMock);
-
 function montarMenu(itensMenu) {
     const cardapio = ["pratosSalgados", "pratosDoces", "bebidasNãoAlcoolicas", "bebidasAlcoolicas"]
     cardapio.forEach((itemCardapio) => {
